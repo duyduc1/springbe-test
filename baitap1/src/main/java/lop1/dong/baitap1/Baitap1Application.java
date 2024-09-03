67111ea56797da414e0832cad85d4a92cced9f2b
duc <duc53044@donga.edu.vn>
2024-09-03 09:32:34 +0700

    add(fearture)

---
+++ baitap1/src/main/java/lop1/dong/baitap1/Baitap1Application.java
@@ -0,0 +1,13 @@
+package lop1.dong.baitap1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Baitap1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Baitap1Application.class, args);
+	}
+
+}
